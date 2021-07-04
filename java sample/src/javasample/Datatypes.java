ada828d7acbe2d2eab728eb210322df796191a8a
meghamala gunnam <meghagunnam2@gmail.com>
2021-07-04 17:23:26 +0530

    first commit
    uploading all files

---
+++ java sample/src/javasample/Datatypes.java
@@ -0,0 +1,22 @@
+package javasample;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		float b = 133.56f;
+		boolean logit = true;
+		String c = "MEGHAMALA";
+		char d = 'G';
+		byte e = 56;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(logit);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+
+	}
+
+}
---
+++ java sample/src/javasample/variables.java
@@ -0,0 +1,16 @@
+package javasample;
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
